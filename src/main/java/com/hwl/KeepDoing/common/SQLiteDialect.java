d1077ba97057c2736ddc806c78540d4085b8db36
hjj6436@163.com <hjj6436@163.com>
2026-03-26 10:03:30 +0800

    添加自定义SQLiteDialect类，解决SQLite方言问题

---
+++ src/main/java/com/hwl/KeepDoing/common/SQLiteDialect.java
@@ -0,0 +1,161 @@
+package com.hwl.KeepDoing.common;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.dialect.function.VarArgsSQLFunction;
+import org.hibernate.type.StringType;
+
+import java.sql.Types;
+
+public class SQLiteDialect extends Dialect {
+    public SQLiteDialect() {
+        super();
+        registerColumnType(Types.BIT, "integer");
+        registerColumnType(Types.TINYINT, "integer");
+        registerColumnType(Types.SMALLINT, "integer");
+        registerColumnType(Types.INTEGER, "integer");
+        registerColumnType(Types.BIGINT, "integer");
+        registerColumnType(Types.FLOAT, "real");
+        registerColumnType(Types.REAL, "real");
+        registerColumnType(Types.DOUBLE, "real");
+        registerColumnType(Types.NUMERIC, "real");
+        registerColumnType(Types.DECIMAL, "real");
+        registerColumnType(Types.CHAR, "text");
+        registerColumnType(Types.VARCHAR, "text");
+        registerColumnType(Types.LONGVARCHAR, "text");
+        registerColumnType(Types.DATE, "date");
+        registerColumnType(Types.TIME, "time");
+        registerColumnType(Types.TIMESTAMP, "timestamp");
+        registerColumnType(Types.BINARY, "blob");
+        registerColumnType(Types.VARBINARY, "blob");
+        registerColumnType(Types.LONGVARBINARY, "blob");
+        registerColumnType(Types.BLOB, "blob");
+        registerColumnType(Types.CLOB, "text");
+        registerColumnType(Types.BOOLEAN, "integer");
+
+        registerFunction("concat", new VarArgsSQLFunction(StringType.INSTANCE, "'"", "||", "'""));
+        registerFunction("mod", new SQLFunctionTemplate(StringType.INSTANCE, "?1 % ?2"));
+        registerFunction("substr", new StandardSQLFunction("substr", StringType.INSTANCE));
+        registerFunction("substring", new StandardSQLFunction("substr", StringType.INSTANCE));
+    }
+
+    @Override
+    public boolean supportsIdentityColumns() {
+        return true;
+    }
+
+    @Override
+    public boolean hasDataTypeInIdentityColumn() {
+        return false;
+    }
+
+    @Override
+    public String getIdentityColumnString() {
+        return "integer";
+    }
+
+    @Override
+    public String getIdentitySelectString() {
+        return "select last_insert_rowid()";
+    }
+
+    @Override
+    public boolean supportsLimit() {
+        return true;
+    }
+
+    @Override
+    public String getLimitString(String query, int offset, int limit) {
+        if (offset > 0) {
+            return query + " limit " + limit + " offset " + offset;
+        } else {
+            return query + " limit " + limit;
+        }
+    }
+
+    @Override
+    public boolean supportsTemporaryTables() {
+        return true;
+    }
+
+    @Override
+    public String getCreateTemporaryTableString() {
+        return "create temporary table if not exists";
+    }
+
+    @Override
+    public boolean dropTemporaryTableAfterUse() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsCurrentTimestampSelection() {
+        return true;
+    }
+
+    @Override
+    public boolean isCurrentTimestampSelectStringCallable() {
+        return false;
+    }
+
+    @Override
+    public String getCurrentTimestampSelectString() {
+        return "select current_timestamp";
+    }
+
+    @Override
+    public boolean supportsUnionAll() {
+        return true;
+    }
+
+    @Override
+    public boolean hasAlterTable() {
+        return false;
+    }
+
+    @Override
+    public boolean dropConstraints() {
+        return false;
+    }
+
+    @Override
+    public String getAddColumnString() {
+        return "add column";
+    }
+
+    @Override
+    public String getForUpdateString() {
+        return "";
+    }
+
+    @Override
+    public boolean supportsOuterJoinForUpdate() {
+        return false;
+    }
+
+    @Override
+    public String getDropForeignKeyString() {
+        return "";
+    }
+
+    @Override
+    public String getAddForeignKeyConstraintString(String constraintName, String[] foreignKey, String referencedTable, String[] primaryKey, boolean referencesPrimaryKey) {
+        return "";
+    }
+
+    @Override
+    public String getAddPrimaryKeyConstraintString(String constraintName) {
+        return "";
+    }
+
+    @Override
+    public boolean supportsIfExistsBeforeTableName() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsCascadeDelete() {
+        return false;
+    }
+}
