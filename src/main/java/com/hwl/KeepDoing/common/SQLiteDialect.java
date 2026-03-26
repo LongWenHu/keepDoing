e5e9897ff37d1cd4a7bdbea6e831c241e6c0cacd
hjj6436@163.com <hjj6436@163.com>
2026-03-26 10:12:11 +0800

    修复SQLiteDialect类，移除不支持的方法覆盖

--- src/main/java/com/hwl/KeepDoing/common/SQLiteDialect.java
+++ src/main/java/com/hwl/KeepDoing/common/SQLiteDialect.java
@@ -1,32 +1,16 @@
 package com.hwl.KeepDoing.common;
 
 import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.identity.IdentityColumnSupport;
-import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
 
 public class SQLiteDialect extends Dialect {
-    @Override
-    public IdentityColumnSupport getIdentityColumnSupport() {
-        return new IdentityColumnSupportImpl() {
-            @Override
-            public boolean supportsIdentityColumns() {
-                return true;
-            }
-
-            @Override
-            public String getIdentitySelectString(String table, String column, int type) {
-                return "select last_insert_rowid()";
-            }
-
-            @Override
-            public String getIdentityColumnString(int type) {
-                return "integer";
-            }
-        };
-    }
 
     @Override
     public boolean supportsLimit() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsLimitOffset() {
         return true;
     }
 
